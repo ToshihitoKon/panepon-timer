7669a0e5e755886172d5b6bdf9d5d5cb2b039246
Toshihito Kon <toshihito.kon@gmail.com>
2021-06-09 09:04:09 +0900

    pr welcome

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Timer />
+  <footer>
+    <a href="https://github.com/ToshihitoKon/panepon-timer">github</a>
+  </footer>
 </template>
 
 <script>
